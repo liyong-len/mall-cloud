d0fa437492a407a661af5e0acff24563057d9af6
liyong <Yong12_Li.sgmext@SAIC-GM.com>
2020-09-25 17:31:21 +0800

    1、增加定时任务

---
+++ src/main/java/com/len/mall/config/SpringTaskConfig.java
@@ -0,0 +1,13 @@
+package com.len.mall.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * 定时任务配置
+ * @author 96490
+ */
+@Configuration
+@EnableScheduling
+public class SpringTaskConfig {
+}
